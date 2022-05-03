d41ecb438db6805dcbe13c036428d30dda5ce62e
Savio Martins <61417912+Savio2552@users.noreply.github.com>
2022-05-02 22:32:51 -0300

    SignUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Home from './pages/Home';
+import SignUp  from './pages/NewAccount';
+import {Route, BrowserRouter, Routes } from 'react-router-dom'
 
 
 function App() {
-  return <Home/>
+  return (
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/SignUp' element={<SignUp/> }/>
+    </Routes>
+    </BrowserRouter>
 
-
-
-
+  );
 }
 export default App;
--- src/pages/NewAccount.tsx
+++ src/pages/NewAccount.tsx
@@ -1,45 +1,45 @@
 import React from 'react';
 import backgroundLogo from '../assets/images/background.svg';
 import '../styles/home.scss'
-import {HiOutlineMail} from 'react-icons/hi'
+
 import {Link, useNavigate} from 'react-router-dom'
 
-function handleSignUp () {
-
-}
 
 
-function Home () {
+
+function SignUp () {
     return(
         <div id='page-auth'>
             <main>
                 <div className='main-content'>
                     <h1>Seja bem vindo!</h1>
-                    <h2>Faça login abaixo.</h2>
+                    <h2>Registre-se abaixo.</h2>
 
                     <form action="#">
+                    <input 
+                        className='input-text'
+                         type="text" 
+                        placeholder='name' />
+
                         <input
                         className='input-text'
                          type="text" 
-                        placeholder='Email'/>   
-
+                        placeholder='Email' />
                         <input
                         className='input-text'
                          type="text" 
                         placeholder='Password' />
-                        
                         <div>
                             <div className='inp-container'>
                             <input 
                             type="radio" />
                             <strong>Lembra-se</strong>
                             </div>
-                            <a>Esqueci minha senha</a>
                         </div>
                         <button type='submit'>
-                            Login
+                        Registre-se
                         </button>
-                        <div className='text-container'> <strong>  Não possui uma conta?   <Link to={'/SignUp'}> Registre-se </Link>   agora!  </strong></div>
+                        <div className='text-container'> <strong>  Já possui uma conta?  Faça <Link to={'/'}> Login </Link>   agora!  </strong></div>
                     </form>
                 </div>
             </main>
@@ -52,4 +52,4 @@ function Home () {
     );
 }
 
-export default Home;
+export default SignUp;
